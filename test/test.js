f29e7e94cc8461bf2b94bde1ed9d6184b0e1bef5
oluwafemi <oluwafemimartins1234@gmail.com>
2024-07-01 06:17:08 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('GET /', () => {
+  it('should return Hello, World!', (done) => {
+    chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.text.should.equal('Hello, World!');
+          done();
+        });
+  });
+});
